f7978f41cf3629677af32f9811302bfb3693013e
Lennon <lennonemp@gmail.com>
2025-10-08 23:29:41 -0300

    feat(team): create initial team-service structure

---
+++ backend/services/team-service/team/src/test/java/com/football/team/TeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

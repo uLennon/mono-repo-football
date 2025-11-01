f5523128fa7bb6e5be5ffae3de468656a72883dd
Lennon <lennonemp@gmail.com>
2025-11-01 17:08:28 -0300

    fix

--- backend/services/team-service/team/src/main/java/com/football/team/model/Team.java
+++ backend/services/team-service/team/src/main/java/com/football/team/model/Team.java
@@ -1,6 +1,5 @@
 package com.football.team.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
